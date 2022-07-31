e7f311012473f04337baf63c99c1104a26c2e386
KaiN <kain@piwnica.ws>
2022-07-31 11:38:45 +0200

    annotations done for ACE's ptplayer C rewrite

--- ptplayer.asm
+++ ptplayer.asm
@@ -20,86 +20,9 @@
 ; Exported functions and variables:
 ; (CUSTOM is the custom-chip register set base address $dff000.)
 ;
-; _mt_install_cia(a6=CUSTOM, a0=AutoVecBase, d0=PALflag.b)
-;   Install a CIA-B interrupt for calling _mt_music or mt_sfxonly.
-;   The music module is replayed via _mt_music when _mt_Enable is non-zero.
-;   Otherwise the interrupt handler calls mt_sfxonly to play sound
-;   effects only.
-;
-; _mt_remove_cia(a6=CUSTOM)
-;   Remove the CIA-B music interrupt and restore the old vector.
-;
-; _mt_init(a6=CUSTOM, a0=TrackerModule, a1=Samples|NULL, d0=InitialSongPos.b)
-;   Initialize a new module.
-;   Reset speed to 6, tempo to 125 and start at the given position.
-;   Master volume is at 64 (maximum).
-;   When a1 is NULL the samples are assumed to be stored after the patterns.
-;
-; _mt_end(a6=CUSTOM)
-;   Stop playing current module.
-;
-; _mt_soundfx(a6=CUSTOM, a0=SamplePointer,
-;            d0=SampleLength.w, d1=SamplePeriod.w, d2=SampleVolume.w)
-;   Request playing of an external sound effect on the most unused channel.
-;   This function is for compatibility with the old API only!
-;   You should call _mt_playfx instead.
-;
-; _mt_playfx(a6=CUSTOM, a0=SfxStructurePointer)
-;   Request playing of a prioritized external sound effect, either on a
-;   fixed channel or on the most unused one.
-;   Structure layout of SfxStructure:
-;     APTR sfx_ptr (pointer to sample start in Chip RAM, even address)
-;     WORD sfx_len (sample length in words)
-;     WORD sfx_per (hardware replay period for sample)
-;     WORD sfx_vol (volume 0..64, is unaffected by the song's master volume)
-;     BYTE sfx_cha (0..3 selected replay channel, -1 selects best channel)
-;     BYTE sfx_pri (unsigned priority, must be non-zero)
-;   When multiple samples are assigned to the same channel the lower
-;   priority sample will be replaced. When priorities are the same, then
-;   the older sample is replaced.
-;   The chosen channel is blocked for music until the effect has
-;   completely been replayed.
-;
-; _mt_musicmask(a6=CUSTOM, d0=ChannelMask.b)
-;   Set bits in the mask define which specific channels are reserved
-;   for music only. Set bit 0 for channel 0, ..., bit 3 for channel 3.
-;   When calling _mt_soundfx or _mt_playfx with automatic channel selection
-;   (sfx_cha=-1) then these masked channels will never be picked.
-;   The mask defaults to 0.
-;
-; _mt_mastervol(a6=CUSTOM, d0=MasterVolume.w)
-;   Set a master volume from 0 to 64 for all music channels.
-;   Note that the master volume does not affect the volume of external
-;   sound effects (which is desired).
-;
-; _mt_music(a6=CUSTOM)
-;   The replayer routine. Is called automatically after mt_install_cia().
-;
-; Variables:
-;
-; _mt_Enable
-;   Set this byte to non-zero to play music, zero to pause playing.
-;   Note that you can still play sound effects, while music is stopped.
-;
-; _mt_E8Trigger
-;   This byte reflects the value of the last E8 command.
-;   It is reset to 0 after mt_init().
-;
-; _mt_MusicChannels
-;   This byte defines the number of channels which should be dedicated
-;   for playing music. So sound effects will never use more
-;   than 4 - _mt_MusicChannels channels at once. Defaults to 0.
-;
 
 		include	"custom.i"
 		include	"cia.i"
-
-
-; Audio channel registers
-AUDLC		equ	0
-AUDLEN		equ	4
-AUDPER		equ	6
-AUDVOL		equ	8
 
 
 ; Sound effects structure, passed into _mt_playfx
@@ -169,6 +92,11 @@ n_sizeof	rs.b	0
 
 
 ;---------------------------------------------------------------------------
+; _mt_install_cia(a6=CUSTOM, a0=AutoVecBase, d0=PALflag.b)
+;   Install a CIA-B interrupt for calling _mt_music or mt_sfxonly.
+;   The music module is replayed via _mt_music when _mt_Enable is non-zero.
+;   Otherwise the interrupt handler calls mt_sfxonly to play sound
+;   effects only.
 	xdef	_mt_install_cia
 _mt_install_cia:
 ; Install a CIA-B interrupt for calling _mt_music.
@@ -177,8 +105,8 @@ _mt_install_cia:
 ; d0 = PALflag.b (0 is NTSC)
 
 	ifnd	SDATA
-	move.l	a4,-(sp)
-	lea	mt_data(pc),a4
+	move.l	a4,-(sp) ;;; push a4 to SP
+	lea	mt_data(pc),a4 ;;; mt_data ptr to a4
 	endc
 
 	clr.b	mt_Enable(a4)
@@ -241,6 +169,8 @@ mt_Lev6Int:
 
 
 ;---------------------------------------------------------------------------
+; _mt_remove_cia(a6=CUSTOM)
+;   Remove the CIA-B music interrupt and restore the old vector.
 	xdef	_mt_remove_cia
 _mt_remove_cia:
 ; Remove CIA-B music interrupt and restore the old vector.
@@ -399,6 +329,11 @@ mt_TimerBsetrep:
 
 
 ;---------------------------------------------------------------------------
+; _mt_init(a6=CUSTOM, a0=TrackerModule, a1=Samples|NULL, d0=InitialSongPos.b)
+;   Initialize a new module.
+;   Reset speed to 6, tempo to 125 and start at the given position.
+;   Master volume is at 64 (maximum).
+;   When a1 is NULL the samples are assumed to be stored after the patterns.
 	xdef	_mt_init
 _mt_init:
 ; Initialize new module.
@@ -490,7 +425,7 @@ mt_reset:
 	move.w	#$0100,mt_chan2+n_intbit(a4)
 	move.w	#$0200,mt_chan3+n_intbit(a4)
 	move.w	#$0400,mt_chan4+n_intbit(a4)
-	move.w	#AUD0LC,mt_chan1+n_audreg(a4)
+	move.w	#AUD0LC,mt_chan1+n_audreg(a4) ;;; NOTE: Unused?
 	move.w	#AUD1LC,mt_chan2+n_audreg(a4)
 	move.w	#AUD2LC,mt_chan3+n_audreg(a4)
 	move.w	#AUD3LC,mt_chan4+n_audreg(a4)
@@ -517,6 +452,8 @@ mt_reset:
 
 
 ;---------------------------------------------------------------------------
+; _mt_end(a6=CUSTOM)
+;   Stop playing current module.
 	xdef	_mt_end
 _mt_end:
 ; Stop playing current module.
@@ -539,6 +476,11 @@ _mt_end:
 
 
 ;---------------------------------------------------------------------------
+; _mt_soundfx(a6=CUSTOM, a0=SamplePointer,
+;            d0=SampleLength.w, d1=SamplePeriod.w, d2=SampleVolume.w)
+;   Request playing of an external sound effect on the most unused channel.
+;   This function is for compatibility with the old API only!
+;   You should call _mt_playfx instead.
 	xdef	_mt_soundfx
 _mt_soundfx:
 ; Request playing of an external sound effect on the most unused channel.
@@ -562,6 +504,22 @@ _mt_soundfx:
 
 
 ;---------------------------------------------------------------------------
+; _mt_playfx(a6=CUSTOM, a0=SfxStructurePointer)
+;   Request playing of a prioritized external sound effect, either on a
+;   fixed channel or on the most unused one.
+;   Structure layout of SfxStructure:
+;     APTR sfx_ptr (pointer to sample start in Chip RAM, even address)
+;     WORD sfx_len (sample length in words)
+;     WORD sfx_per (hardware replay period for sample)
+;     WORD sfx_vol (volume 0..64, is unaffected by the song's master volume)
+;     BYTE sfx_cha (0..3 selected replay channel, -1 selects best channel)
+;     BYTE sfx_pri (unsigned priority, must be non-zero)
+;   When multiple samples are assigned to the same channel the lower
+;   priority sample will be replaced. When priorities are the same, then
+;   the older sample is replaced.
+;   The chosen channel is blocked for music until the effect has
+;   completely been replayed.
+
 	xdef	_mt_playfx
 _mt_playfx:
 ; Request playing of a prioritized external sound effect, either on a
@@ -851,6 +809,12 @@ exit_playfx:
 
 
 ;---------------------------------------------------------------------------
+; _mt_musicmask(a6=CUSTOM, d0=ChannelMask.b)
+;   Set bits in the mask define which specific channels are reserved
+;   for music only. Set bit 0 for channel 0, ..., bit 3 for channel 3.
+;   When calling _mt_soundfx or _mt_playfx with automatic channel selection
+;   (sfx_cha=-1) then these masked channels will never be picked.
+;   The mask defaults to 0.
 	xdef	_mt_musicmask
 _mt_musicmask:
 ; Set bits in the mask define which specific channels are reserved
@@ -883,6 +847,10 @@ _mt_musicmask:
 
 
 ;---------------------------------------------------------------------------
+; _mt_mastervol(a6=CUSTOM, d0=MasterVolume.w)
+;   Set a master volume from 0 to 64 for all music channels.
+;   Note that the master volume does not affect the volume of external
+;   sound effects (which is desired).
 	xdef	_mt_mastervol
 _mt_mastervol:
 ; Set a master volume from 0 to 64 for all music channels.
@@ -912,6 +880,8 @@ _mt_mastervol:
 
 
 ;---------------------------------------------------------------------------
+; _mt_music(a6=CUSTOM)
+;   The replayer routine. Is called automatically after mt_install_cia().
 	xdef	_mt_music
 _mt_music:
 ; Called from interrupt.
@@ -1267,23 +1237,23 @@ mt_playvoice:
 	and.b	n_cmd(a2),d5
 	add.w	d5,d5			; d5 cmd*2
 
-	moveq	#0,d4
-	move.b	d6,d4			; d4 cmd argument (in MSW)
+	moveq	#0,d4                                      ;;; d6: note:16, cmdHi:8, cmdLo:8
+	move.b	d6,d4			; d4 cmd argument (in MSW)     ;;; d4 := cmdLo
 	swap	d4
-	move.w	#$0ff0,d4
-	and.w	d6,d4			; d4 for checking E-cmd (in LSW)
+	move.w	#$0ff0,d4                                ;;; d4 := empty:8 cmdLo:8 0F F0
+	and.w	d6,d4			; d4 for checking E-cmd (in LSW) ;;; d4 := empty:8 cmdLo:8 (cmdHi&0F):8 (cmdLo&F0):8
 
-	swap	d6
-	move.l	d6,d0			; S...S...
-	clr.b	d0
-	rol.w	#4,d0
-	rol.l	#4,d0			; ....00SS
+	swap	d6                     ;;; d6: cmdHi:8 cmdLo:8 note:16
+	move.l	d6,d0			; A...B... ;;; d0: cmdHi:8 cmdLo:8 note:16
+	clr.b	d0                     ;;; d0: cmdHi:8 cmdLo:8 noteHi:8 empty:8
+	rol.w	#4,d0       ; A......B ;;; d0: cmdHi:8 cmdLo:8 noteHiLo:4 empty:8 noteHiHi:4
+	rol.l	#4,d0			  ; ....00BA ;;; d0: cmdHiLo:4 cmdLo:8 noteHiLo:4 empty:8 noteHiHi:4 cmdHiHi:4
 
-	and.w	#$0fff,d6		; d6 note
+	and.w	#$0fff,d6		; d6 note  ;;; d6: cmdHi:8 cmdLo:8 empty:4 note:12
 
-	; get sample start address
+	; get sample start address   ;;; BA is a sample number
 	add.w	d0,d0			; sample number * 2
-	beq	set_regs
+	beq	set_regs                 ;;; set_regs(d4, d5, d6)
 	move.w	mult30tab(pc,d0.w),d1	; d1 sample info table offset
 	lea	mt_SampleStarts(a4),a0
 	add.w	d0,d0
@@ -1303,8 +1273,8 @@ mt_playvoice:
 
 	; determine period table from fine-tune parameter
 	moveq	#0,d3
-	move.b	(a0)+,d3
-	add.w	d3,d3
+	move.b	(a0)+,d3 ;;; d3 := nibble
+	add.w	d3,d3      ;;; nibble *= 2
 	move.l	a0,d1
 	lea	mt_PerFineTune(pc),a0
 	add.w	(a0,d3.w),a0
@@ -1348,7 +1318,7 @@ set_loop:
 set_regs:
 ; d4 = cmd argument | masked E-cmd
 ; d5 = cmd*2
-; d6 = cmd.w | note.w
+; d6 = cmd.w | note.w:12
 
 	move.w	d4,d3			; d3 masked E-cmd
 	swap	d4			; d4 cmd argument into LSW
@@ -1432,7 +1402,7 @@ set_finetune:
 	add.w	d0,d0
 	add.w	(a0,d0.w),a0
 	move.l	a0,n_pertab(a2)
-	cmp.w	#32,d0
+	cmp.w	#32,d0 ;;; can it trigger? max: 0xF * 2 = 30, always smaller than 32
 	shs	n_minusft(a2)
 
 set_period:
@@ -1651,10 +1621,10 @@ mt_toneporta_nc:
 
 	; glissando: find nearest note for new period
 	move.l	n_pertab(a2),a0
-	moveq	#36-1,d0
-	moveq	#-2,d1
-.4:	addq.w	#2,d1
-	cmp.w	(a0)+,d2
+	moveq	#36-1,d0  ;;; d0 := 35
+	moveq	#-2,d1    ;;; d1 := -2
+.4:	addq.w	#2,d1 ;;; d1 += 2
+	cmp.w	(a0)+,d2  ;;; if (d2 < *(a0++))) { if (--d0 != -1) goto .4 } wtf mate
 	dbhs	d0,.4
 
 	move.w	d1,n_noteoff(a2)	; @@@ needed?
@@ -1721,8 +1691,8 @@ mt_tonevolslide:
 ; cmd 5 x y (x = volume-up, y = volume-down)
 ; d4 = xy
 
-	pea	mt_volumeslide(pc)
-	bra	mt_toneporta_nc
+	pea	mt_volumeslide(pc) ;;; Push this fn to call stack
+	bra	mt_toneporta_nc    ;;; so that this fn returns to mt_volumeslide
 
 
 mt_vibrvolslide:
@@ -1867,12 +1837,12 @@ mt_patternbrk:
 	move.w	d4,d1
 	lsr.w	#4,d1
 	add.b	mult10tab(pc,d1.w),d0
-	cmp.b	#63,d0
-	bhi	jump_pos0
+	cmp.b	#63,d0                ;;; d0 - 63
+	bhi	jump_pos0               ;;; d0 > 63 -> goto jump_pos0
 
-	lsl.w	#4,d0
-	move.w	d0,mt_PBreakPos(a4)
-	st	mt_PosJumpFlag(a4)
+	lsl.w	#4,d0                 ;;; d0 <<= 4
+	move.w	d0,mt_PBreakPos(a4) ;;; mt_pbreakPos[a4] := d0
+	st	mt_PosJumpFlag(a4)      ;;; mt_PosJumpFlag(a4) := 0xFFFFFFFF
 	rts
 
 mult10tab:
@@ -2001,7 +1971,7 @@ mt_finetune:
 	add.w	d0,d0
 	add.w	(a0,d0.w),a0
 	move.l	a0,n_pertab(a2)
-	cmp.w	#32,d0
+	cmp.w	#32,d0      ;;; Since arg is on 4 bits it can only take values 0..15, 2*d0 is up to 30, and it's always smaller than 32
 	shs	n_minusft(a2)
 	rts
 
@@ -2059,7 +2029,7 @@ mt_retrignote:
 	beq	.2
 
 	; set new retrigger count when Counter=0
-.1:	tst.b	mt_Counter(a4)
+.1:	tst.b	mt_Counter(a4) ;;; What is the label '.1' here for?
 	bne	.3
 	move.b	d0,n_retrigcount(a2)
 
@@ -2161,20 +2131,20 @@ mt_updatefunk:
 	move.b	mt_FunkTable(pc,d0.w),d0
 	add.b	d0,n_funkoffset(a2)
 	bpl	.2
-	move.b	d7,n_funkoffset(a2)
 
-	move.l	n_loopstart(a2),d0
-	moveq	#0,d1
-	move.w	n_replen(a2),d1
-	add.l	d1,d1
-	add.l	d0,d1
-	move.l	n_wavestart(a2),a0
-	addq.l	#1,a0
-	cmp.l	d1,a0
+	move.b	d7,n_funkoffset(a2)  ;;; n_funkoffset := 0
+	move.l	n_loopstart(a2),d0   ;;; d0 := n_loopstart
+	moveq	#0,d1                  ;;; d1 := 0
+	move.w	n_replen(a2),d1      ;;; d1 := n_replen
+	add.l	d1,d1                  ;;; d1 *= 2
+	add.l	d0,d1                  ;;; d1 += d0
+	move.l	n_wavestart(a2),a0   ;;; ptr.a0 = ptr.n_wavestart
+	addq.l	#1,a0                ;;; ptr.a0 += 1
+	cmp.l	d1,a0                  ;;; a0 >= d1 --> a0 := d0
 	blo	.1
 	move.l	d0,a0
-.1:	move.l	a0,n_wavestart(a2)
-	not.b	(a0)
+.1:	move.l	a0,n_wavestart(a2) ;;; n_wavestart := a0
+	not.b	(a0)                   ;;; *a0 := a0 ^ 0xFF
 
 .2:	rts
 
@@ -2900,16 +2870,26 @@ mt_PattDelTime2:
 mt_SilCntValid:
 	ds.b	1
 
+; _mt_Enable
+;   Set this byte to non-zero to play music, zero to pause playing.
+;   Note that you can still play sound effects, while music is stopped.
 	xdef	_mt_Enable
 _mt_Enable:
 mt_Enable:
 	ds.b	1
 
+; _mt_E8Trigger
+;   This byte reflects the value of the last E8 command.
+;   It is reset to 0 after mt_init().
 	xdef	_mt_E8Trigger
 _mt_E8Trigger:
 mt_E8Trigger:
 	ds.b	1
 
+; _mt_MusicChannels
+;   This byte defines the number of channels which should be dedicated
+;   for playing music. So sound effects will never use more
+;   than 4 - _mt_MusicChannels channels at once. Defaults to 0.
 	xdef	_mt_MusicChannels
 _mt_MusicChannels:
 mt_MusicChannels:
